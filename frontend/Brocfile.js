fdade512ae65ad2bc042ee804f2283a2bb99e42f
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2015-04-10 10:30:09 +0200

    Added bootstrap and changed style to less

--- frontend/Brocfile.js
+++ frontend/Brocfile.js
@@ -1,22 +1,39 @@
+/* global require, module */
+
 var esTranspiler = require('broccoli-babel-transpiler')
 var funnel       = require('broccoli-funnel')
 var mergeTrees   = require('broccoli-merge-trees')
 var concat       = require('broccoli-concat')
 var uglify       = require('broccoli-uglify-js')
+var compileLess  = require('broccoli-less-single')
+var cleanCss     = require('broccoli-clean-css')
 
-var app = funnel('js')
+var appJsTree   = funnel('js')
+var appLessTree = funnel('less')
+var vendorTree  = funnel('bower_components')
 
-var vendor = concat('bower_components', {
-  inputFiles: [
-    'EaselJS/lib/easeljs-0.8.0.combined.js',
-    'SoundJS/lib/soundjs-0.6.0.combined.js',
-    'jquery/dist/jquery.js',
-    'socket.io-client/socket.io.js',
-  ],
-  outputFile: '/vendor.js'
-})
+var appCss    = cleanCss(compileLess(appLessTree, 'app.less', 'app.css'))
+var vendorCss = cleanCss(compileLess(vendorTree, 'bootstrap/less/bootstrap.less', 'vendor.css'))
 
-vendor = uglify(vendor, { mangle: true, compress: true })
-var scriptTree = esTranspiler(app, {})
+var vendorJs = uglify(
+  concat(
+    vendorTree, {
+      inputFiles: [
+        'EaselJS/lib/easeljs-0.8.0.combined.js'
+      , 'SoundJS/lib/soundjs-0.6.0.combined.js'
+      , 'jquery/dist/jquery.js'
+      , 'socket.io-client/socket.io.js'
+      , 'Keypress/keypress.js'
+      ]
+    , outputFile: '/vendor.js'
+    }
+  )
+, {
+    mangle: true
+  , compress: true
+  }
+)
 
-module.exports = mergeTrees([ scriptTree, vendor ])
+var appJs = esTranspiler(appJsTree, {})
+
+module.exports = mergeTrees([ appCss, appJs, vendorJs, vendorCss ])
